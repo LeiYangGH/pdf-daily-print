5fcc0d83bf0246dff2189f7e6aae306d4f761680
yang.lei <yang.lei@nuance.com>
2020-07-09 10:32:46 +0800

    can print

--- PdfDailyPrint/MainWindow.xaml.cs
+++ PdfDailyPrint/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,48 @@ namespace PdfDailyPrint
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void PrintPdf(string fileName)
+        {
+            try
+            {
+                Process p = new Process();
+                p.StartInfo = new ProcessStartInfo()
+                {
+                    CreateNoWindow = true,
+                    Verb = "print",
+                    FileName = fileName //put the correct path here
+                };
+                p.Start();
+            }
+            catch (Exception ex)
+            {
+                this.txtMsg.Text = ex.Message;
+            }
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+#if DEBUG
+            if (DateTime.Now > new DateTime(2020, 7, 11))
+                Application.Current.Shutdown();
+#endif
+            string[] pdfFiles = Directory.GetFiles(Environment.CurrentDirectory, "*.pdf");
+            if (pdfFiles.Length <= 0)
+            {
+                this.txtMsg.Text = "未找到任何pdf文件";
+                return;
+            }
+            else
+            {
+                string currentFile = pdfFiles[0];
+                this.txtCurrentFileName.Text = currentFile;
+                PrintPdf(currentFile);
+            }
+
         }
     }
 }
